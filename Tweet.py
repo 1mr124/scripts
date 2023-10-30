c12867c85e892301ed8aad60564933cb48e9ec80
1Mr12 <pubg.twitter12@gmail.com>
2023-10-30 23:30:20 +0200

    continue sending option

--- Tweet.py
+++ Tweet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import tweepy
-from sys import argv
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -11,6 +10,23 @@ from cryptography.fernet import Fernet
 import base64
 import json
 import getpass 
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='This is a Test',add_help=False)
+
+parser.add_argument('-c', '--continuee', help='this will continuse sending tweets')
+parser.add_argument('-s', '--send', help='this will send single tweet')
+parser.add_argument('--help', '-h', action='help', help='-m for morese code \n-b for binary code')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Access the values of the arguments
+continueeMode = args.continuee
+send = args.send
+
 
 def generate_key(password):
     salt = b'HopeIsLife'  # Salt for key derivation
@@ -44,6 +60,23 @@ def loadData(filename):
         print(error)
         return False
 
+def sendTweet(client, tweet):
+    print("sending ",tweet)
+    response = client.create_tweet(text=tweet)
+    print("Tweet posted successfully!")
+
+def continueeSendingTweet(client):
+    Exitflag = False 
+    while not(Exitflag):
+        tweet = input("Tweet That: ")
+        if tweet == "exit" or tweet == "break":
+            Exitflag = True
+        else:
+            response = client.create_tweet(text=tweet)
+            print("Tweet posted successfully!")
+
+
+
 if __name__ == "__main__":
     # Set up your Twitter API credentials
     filename = "/home/mr124/Documents/.TwitterApi.bin"
@@ -54,15 +87,9 @@ if __name__ == "__main__":
     data = json.loads(data)
     if data:
         client = tweepy.Client(consumer_key=data["Mr12ConsumerKey"], consumer_secret=data["Mr12ConsumerSecret"], access_token=data["Mr12AccessToken"], access_token_secret=data["Mr12AccessTokenSecret"])
-        try:
-            tweet = argv[1]
-            print("sending ",tweet)
-            response = client.create_tweet(text=tweet)
-            print("Tweet posted successfully!")
-        except:
-            print("error")
-    else :
-        print("Error decrypting data")
-
-
-
+        if send:
+            sendTweet(client, args.send)
+        elif continueeMode:
+            continueeSendingTweet(client)         
+    else:
+        print("error on data")
