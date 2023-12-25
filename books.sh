7d0b273757965bc3502b93b5de5c87158b4ac22a
1Mr12 <pubg.twitter12@gmail.com>
2023-12-25 04:41:02 +0200

    current books

---
+++ books.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Directory where your PDF books are stored
+booksDirectory="/home/mr124/Documents/CurrentReading"
+
+# Use find to locate PDF files in subfolders
+BooksNames=$(find "$booksDirectory" -iname '*.pdf' | rev | cut -d '/' -f 1 | rev | cut -d '.' -f 1)
+
+# Use dmenu to create a dynamic menu from the list of PDF files
+selectedFile=$(echo "$BooksNames" | dmenu -p "Select a book:")
+
+# Check if the user selected a book
+if [ -n "$selectedFile" ]; then
+    PdfPath=$(find "$booksDirectory" -type f -iname "$selectedFile*" )
+    echo $PdfPath 
+    zathura "$PdfPath" &
+fi
