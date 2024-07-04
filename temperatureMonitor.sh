3a7dc74dda0a0a5cb53c2a028c3412252d94dae0
Mr12 <221bst221@gmail.com>
2024-07-04 22:04:30 +0300

    the script works for both Home, Lab

--- temperatureMonitor.sh
+++ temperatureMonitor.sh
@@ -1,15 +1,20 @@
 #!/bin/zsh
 
-# Get CPU temperature
-GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
+hostname=$(hostname)
 
-
-# Get GPU temperature
-CPU_TEMP=$(sensors | grep 'Core' | awk '{print int($3)}' | paste -sd ',')
-
-# Default values if not found
-#[ -z "$CPU_TEMP" ] && CPU_TEMP="N/A"
-#[ -z "$GPU_TEMP" ] && GPU_TEMP="N/A"
-
-# Print the output
-echo "CPU:"$CPU_TEMP"|""GPU:"$GPU_TEMP 🥵
+case $hostname in
+  "Home")
+    GPU_TEMP=$(sensors | grep -i 'edge:' | awk '{print $2}' | tr -d '+°C')
+    CPU_TEMP=$(sensors | grep -i 'temp1:' | awk '{print substr($2, 1, length($2)-2)}' | tr -d '+' | paste -sd ',' -)
+    echo "CPU:"$CPU_TEMP"|""GPU:"$GPU_TEMP 🥵
+    ;;
+  "Mr12Lab")
+    GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
+    CPU_TEMP=$(sensors | grep 'Core' | awk '{print int($3)}' | paste -sd ',')
+    echo "CPU:"$CPU_TEMP"|""GPU:"$GPU_TEMP 🥵
+    ;;
+  *)
+    echo "Unknown hostname. Performing default task..."
+    # Add your default commands here
+    ;;
+esac
